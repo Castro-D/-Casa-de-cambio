7289c86342b56846e0b0f55b729ecc78f2a5b37a
Castro-D <castro.dh91@gmail.com>
2021-04-22 15:56:17 -0300

    agrega opciones de monedas

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ fetch('https://api.ratesapi.io/api/latest')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error("FALLO", error))
-  
+
+document.querySelector('button').onclick = function (){
+ 
+}
