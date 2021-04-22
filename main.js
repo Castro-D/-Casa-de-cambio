4fc02ab6f13cb99edb062b18a90c911c720aaa35
Castro-D <castro.dh91@gmail.com>
2021-04-22 18:18:29 -0300

    recolecta datos del api

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-fetch('https://api.ratesapi.io/api/latest')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error("FALLO", error))
 
 document.querySelector('button').onclick = function (){
+  const $moneda = document.querySelector('#bases').value;
+  const $fecha = document.querySelector('#fecha').value;
+  fetch(`https://api.ratesapi.io/api/${$fecha}?base=${$moneda}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("FALLO", error))
+
  
 }
