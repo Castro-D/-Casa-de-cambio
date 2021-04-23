a7144564649a97d19c1cb650e7f870354e4747e9
Castro-D <castro.dh91@gmail.com>
2021-04-23 19:36:16 -0300

    agrega datos a tabla html

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+/// <reference types='jquery' />
 
 document.querySelector('button').onclick = function (){
   const $moneda = document.querySelector('#bases').value;
   const $fecha = document.querySelector('#fecha').value;
   fetch(`https://api.ratesapi.io/api/${$fecha}?base=${$moneda}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data =>{
+      Object.keys(data.rates).forEach(function(key){
+        const $table = $('table')
+        $table.append(`<tr><td>${key}</td><td>${data.rates[key]}</td></tr>`)
+      })
+    })
     .catch(error => console.error("FALLO", error))
 
- 
+  
 }
+
