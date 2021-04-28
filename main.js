df910727163736d889c70d5da10ab03b6783b774
Castro-D <castro.dh91@gmail.com>
2021-04-28 15:26:19 -0300

    agrega funcion resetearTabla
    es necesario que se borren los datos de la tabla anterior cuando se hace una nueva busqueda

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 /// <reference types='jquery' />
 
+let $table = $('table');
+
 document.querySelector('button').onclick = function (){
+  resetearTabla()
   const $moneda = document.querySelector('#bases').value;
   const $fecha = document.querySelector('#fecha').value;
   fetch(`https://api.ratesapi.io/api/${$fecha}?base=${$moneda}`)
     .then(response => response.json())
     .then(data =>{
       Object.keys(data.rates).forEach(function(key){
-        const $table = $('table')
         $table.append(`<tr><td>${key}</td><td>${data.rates[key]}</td></tr>`)
       })
     })
     .catch(error => console.error("FALLO", error))
+}
 
-  
+function resetearTabla(){
+  $table.find("tr:gt(0)").remove();
 }
 
