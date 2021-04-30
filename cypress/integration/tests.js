4025830edce8f691a8cb14520d48241286d534f1
Castro-D <castro.dh91@gmail.com>
2021-04-30 15:37:39 -0300

    testea cantidad de elementos en pagina

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -1,2 +1,14 @@
 /// <reference types='Cypress'/>
 
+const URL = '192.168.0.133:8080';
+const ELEMENTOS_PAGINA = '5';
+
+context('casa de cambio', () => {
+    before(() => {
+        cy.visit(URL);
+    });
+
+    it('tests', () => {
+        cy.get('.row').should('have.length', ELEMENTOS_PAGINA);
+    });
+});
